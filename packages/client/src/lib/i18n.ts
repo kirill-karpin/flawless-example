8a255dfb3585d6386871be88c77f278ed2d3dc6f
Kirill Karpin <kirill-karpin@gitlab.com>
2022-07-01 09:27:14 +0500

    feat: init boilerplate

---
+++ packages/client/src/lib/i18n.ts
@@ -0,0 +1,28 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import * as en from '../lib/lang/en';
+import * as ru from '../lib/lang/ru';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    fallbackLng: 'ru',
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    resources: {
+      ru: {
+        translation: en,
+      },
+      en: {
+        translation: en,
+      },
+    },
+  });
+
+export const useT = () => {};
+
+export default i18n;
